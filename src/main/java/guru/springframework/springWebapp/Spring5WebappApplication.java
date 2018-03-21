9d0f26dc52c6b2804e727f514e006c27ef3c9a92
Jainik Patel <jainikhpatel@yahoo.com>
2018-03-21 08:42:56 +0530

    removed unused import

--- src/main/java/guru/springframework/springWebapp/Spring5WebappApplication.java
+++ src/main/java/guru/springframework/springWebapp/Spring5WebappApplication.java
@@ -2,7 +2,6 @@ package guru.springframework.springWebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Spring5WebappApplication {
